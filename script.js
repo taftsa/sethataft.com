7743026b2dac00cbda0514538f57ef532ef02cca
taftsa <30739359+taftsa@users.noreply.github.com>
2021-07-12 21:10:03 -0400

    0.2

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+var bodyText = {
+	About: "This will be the About section.",
+	Teaching: "This will be the Teaching section.",
+	Playing: "This will be the Playing section.",
+	Researching: "This will be the Researching section.",
+	Coding: "This will be the Coding section.",
+	Woodworking: "This will be the Woodworking section.",
+};
+
+
 $(document).ready(function(){
 
+});
+
+$(document).on('click', '.mainMenu.link', function(){
+	$(this).addClass('current');
+	$(this).children('image').css('opacity', '1');
+	
+	var section = $('.mainMenu.link.current').children('.label').html();
+
+	$('.mainMenu.link.current').children('.image').css('opacity','1');
+	$('.mainMenu.link.current').children('.label').empty();
+	
+	$('.mainMenu.link').not('.current').remove();
+	
+	$('body').append('<h1>' + section + '<h1>');
+	$('body').append('<p>' + bodyText[section] + '<p>');
+	$('body').append('<div id="back"><</div>');
+});
+
+$(document).on('click', '#back', function(){
+	window.location.replace('sethataft.com');
 });
